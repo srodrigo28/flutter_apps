197b867b4403c539c5e7ee60a29f75162769213c
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-08-08 11:56:13 -0300

    atualizado a as pastas

---
+++ despesas/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Versão Inicial'),
+    );
+  }
+}
